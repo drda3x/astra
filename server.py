93d7e429e820fd674f5fbeab7b14a77d69d75cb2
Vasily Nesterov <da3x11@gmail.com>
2016-05-22 16:17:11 +0300

    Определение IP

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ u"""
 Пакет для работы сервера приложения
 """
 
+import socket
+
 
 class SocketServer(object):
     u"""
@@ -15,12 +17,20 @@ class SocketServer(object):
 
         # Создать сокет, выделить его в отдельный поток
         # Настроить общение с сокетом
-        pass
+
+        self.socket = socket.socket()
+        self.socket.connect(("gmail.com", 80))
+        self.host = self.socket.getsockname()[0]
 
     def send_message(self):
         # Отправить сообщение
         pass
 
-    def chack_messages(self):
+    def check_messages(self):
         # Проверить сообщения от клиента
         pass
+
+    def loop(self):
+        # Петелька
+        pass
+
